c774be494634e10fc96dd0e0236fe28974878307
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-09 10:40:50 -0700

    add Models
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

---
+++ Models/TravelApi.cs
@@ -0,0 +1,12 @@
+namespace TravelApi.Models
+{
+  public class Review
+  {
+    public int ReviewId { get; set; }
+    public int Rating { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Body { get; set; }  
+    public string UserName { get; set; }
+  }
+}
